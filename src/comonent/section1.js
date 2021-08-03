2c439e5ea6acbe7db193c64690d1e026dcfaab45
ayushchauhan1 <ayush_c@ar.iitr.ac.in>
2021-08-03 22:42:02 +0530

    first commit

---
+++ src/comonent/section1.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class section1 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi !</h1>
+
+                <h2>svdbmsvksvbksb</h2>
+            </div>
+        )
+    }
+}
+
+export default section1
---
+++ src/comonent/section2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import desoc from "../assets/desoc.svg"
+export class section2 extends Component {
+    render() {
+        return (
+            <div>
+                <img src={desoc}></img>
+            </div>
+        )
+    }
+}
+
+export default section2
